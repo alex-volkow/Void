635a9ec9691628f423391df4e1dd926b880a4807
The Developer <denis.skornyakov@gmail.com>
2019-06-28 16:35:22 +0300

    PropertyExtensions has been added

---
+++ Void.Box/Reflection/PropertyExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void.Reflection
+{
+    public static class PropertyExtensions
+    {
+        public static FieldInfo GetAutoField(this PropertyInfo property) {
+            return property.DeclaringType.GetField(
+                $"<{property.Name}>k__BackingField",
+                BindingFlags.NonPublic |
+                BindingFlags.Instance
+                );
+        }
+
+        public static bool IsAutoProperty(this PropertyInfo property) {
+            return property.GetAutoField() != null;
+        }
+
+        public static void SetForce(this PropertyInfo property, object obj, object value) {
+            if (property.CanWrite) {
+                property.SetValue(obj, value);
+                return;
+            }
+            var autofield = property.GetAutoField();
+            if (autofield != null) {
+                autofield.SetValue(obj, value);
+                return;
+            }
+            property.SetValue(obj, value);
+        }
+    }
+}

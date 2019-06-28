2cbba7a41a0f73e96f6e9cf43c7e3741090a1516
The Developer <denis.skornyakov@gmail.com>
2019-06-28 16:11:42 +0300

    Definitions has been added

---
+++ Void.Box/Collections/IDefinitions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void.Collections
+{
+    public interface IDefinitions : IReadOnlyDictionary<Type, object>
+    {
+        object Get(Type type);
+        object GetRequired(Type type);
+        T Get<T>();
+        T GetRequired<T>();
+    }
+}

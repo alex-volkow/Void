531924f18b2d6224218551a2874b80858059d04c
The Developer <denis.skornyakov@gmail.com>
2019-06-28 16:34:34 +0300

    AssembliesExtensions and StreamsExtensions have been added

---
+++ Void.Box/IO/StreamExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void.IO
+{
+    public static class StreamExtensions
+    {
+        public static async Task<byte[]> ToArrayAsync(this Stream stream) {
+            using (var memory = new MemoryStream()) {
+                if (stream.CanSeek) {
+                    stream.Position = 0;
+                }
+                await stream.CopyToAsync(memory);
+                return memory.ToArray();
+            }
+        }
+
+        public static byte[] ToArray(this Stream stream) {
+            using (var memory = new MemoryStream()) {
+                if (stream.CanSeek) {
+                    stream.Position = 0;
+                }
+                stream.CopyTo(memory);
+                return memory.ToArray();
+            }
+        }
+
+        public static string GetSHA256(this Stream stream) {
+            using (var engine = SHA256Managed.Create()) {
+                return Convert.ToBase64String(engine.ComputeHash(stream));
+            }
+        }
+    }
+}
---
+++ Void.Box/Reflection/AssemblyExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Void.IO;
+
+namespace Void.Reflection
+{
+    public static class AssemblyExtensions
+    {
+        public static string GetPath(this Assembly assembly) {
+            var uri = new UriBuilder(assembly.CodeBase);
+            return Uri.UnescapeDataString(uri.Path);
+        }
+
+        public static string GetNamespace(this Assembly assembly) {
+            return assembly?.ManifestModule?.Name
+                ?.Remove(".dll")
+                ?.Remove(".exe");
+        }
+
+        public static byte[] ReadBytesResource(this Assembly assembly, string name) {
+            var path = assembly.GetNamespace();
+            if (!name.StartsWith(path)) {
+                name = $"{path}.{name}";
+            }
+            using (var stream = assembly.GetManifestResourceStream(name)) {
+                return stream.ToArray();
+            }
+        }
+
+        public static string ReadStringResource(this Assembly assembly, string name) {
+            return assembly.ReadStringResource(name, Encoding.UTF8);
+        }
+
+        public static string ReadStringResource(this Assembly assembly, string name, Encoding encoding) {
+            return encoding.GetString(assembly.ReadBytesResource(name));
+        }
+    }
+}

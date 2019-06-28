3cb014ea20bb047db60982636eb0c5e64f463d34
The Developer <denis.skornyakov@gmail.com>
2019-06-28 17:00:40 +0300

    Void.Windows project has been added

---
+++ Void.Windows/Diagnostics/Cmd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void.Diagnostics
+{
+    public static class Cmd
+    {
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine,
+            out int pNumArgs
+            );
+
+        public static string[] ToArray(string arguments) {
+            var argv = CommandLineToArgvW(arguments, out int argc);
+            if (argv == IntPtr.Zero) {
+                throw new Win32Exception();
+            }
+            try {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++) {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+                return args;
+            }
+            finally {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
+    }
+}

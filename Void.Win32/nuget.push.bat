2559ce96e5ea1bd6cc453d136781c6cee69b4248
The Developer <denis.skornyakov@gmail.com>
2019-06-28 17:48:46 +0300

    Nuget scripts has been added

---
+++ Void.Win32/nuget.push.bat
@@ -0,0 +1,11 @@
+@echo off
+
+REM delete existing nuget packages
+del bin\Release\*.nupkg
+
+dotnet pack Void.Win32.csproj -c Release
+
+REM nuget push \bin\Release\*.nupkg
+dotnet nuget push bin\Release\*.nupkg -k oy2e6jpimkn24iz26liqzw65ad4ijkmexk2dfsjozmys3a -s https://api.nuget.org/v3/index.json
+
+@pause

877ac886da5ea6dd76a2040ab7aaea0030379778
The Developer <denis.skornyakov@gmail.com>
2019-06-28 16:37:23 +0300

    TypeExtensions & test have been added

---
+++ Void.Box.Tests/Reflection/TypeExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Void.Reflection
+{
+    public class TypeExtensionsTests
+    {
+        [Fact]
+        public void IsTest() {
+            Assert.True(typeof(object).Is<object>());
+            Assert.True(typeof(Item).Is<object>());
+            Assert.True(typeof(Item).Is<IItem>());
+            Assert.False(typeof(IItem).Is<Item>());
+            //IList<Item> items1 = new List<Item>();
+            Assert.True(typeof(List<Item>).Is<IList<Item>>());
+            Assert.True(typeof(List<Item>).Is<List<Item>>());
+            Assert.False(typeof(IList<Item>).Is<List<Item>>());
+            //IList<IItem> items2 = new List<IItem>();
+            Assert.True(typeof(List<IItem>).Is<IList<IItem>>());
+            Assert.True(typeof(List<IItem>).Is<List<IItem>>());
+            Assert.False(typeof(IList<IItem>).Is<List<IItem>>());
+            //IItems<IItem> items3 = new Items<Item>();
+            Assert.True(typeof(Items<Item>).Is<IOutItems<IItem>>());
+            Assert.False(typeof(IOutItems<IItem>).Is<Items<Item>>());
+            Assert.False(typeof(Items<Item>).Is<IStdItems<IItem>>());
+            Assert.False(typeof(IStdItems<IItem>).Is<Items<Item>>());
+            ////
+            Assert.True(typeof(List<Item>).Is(typeof(List<>)));
+            Assert.True(typeof(List<IItem>).Is(typeof(List<>)));
+        }
+
+        private class Item : IItem { }
+
+        private interface IItem { }
+
+        private class Items<T> : IOutItems<T> { }
+
+        private interface IOutItems<out T> { }
+
+        private interface IStdItems<T> { }
+
+    }
+}
---
+++ Void.Box/Reflection/TypeExtensions.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Void.Reflection
+{
+    public static class TypeExtensions
+    {
+        private static readonly Regex TYPE_NAME_WITH_NAMESPACES = new Regex(@",\s*[^,\s\[\]]+", RegexOptions.Compiled);
+        private static readonly Regex TYPE_NAME_WITH_ASSEMBLIES = new Regex(@"(,\s*)?((Version)|(Culture)|(PublicKeyToken))=[\w_\.]*", RegexOptions.Compiled);
+
+
+        public static PropertyInfo GetPropertyInfo<TSource, TProperty>(
+            this TSource source,
+            Expression<Func<TSource, TProperty>> property) {
+            var type = typeof(TSource);
+            if (!(property.Body is MemberExpression member)) {
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    property.ToString()
+                    ));
+            }
+            var info = member.Member as PropertyInfo;
+            if (info == null) {
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    property.ToString()
+                    ));
+            }
+            if (type != info.ReflectedType && !type.IsSubclassOf(info.ReflectedType)) {
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a property that is not from type {1}.",
+                    property.ToString(), type
+                    ));
+            }
+            return info;
+        }
+
+        public static string GetAssemblyLocation(this Type type) {
+            return type.Assembly.GetPath();
+        }
+
+        public static string GetNameWithAssemblies(this Type type) {
+            return TYPE_NAME_WITH_ASSEMBLIES.Replace(type.AssemblyQualifiedName, string.Empty);
+        }
+
+        public static string GetNameWithNamespaces(this Type type) {
+            return TYPE_NAME_WITH_NAMESPACES.Replace(type.AssemblyQualifiedName, string.Empty);
+        }
+
+        public static bool IsNullable(this Type type) {
+            return type != null
+                && type.IsGenericType &&
+                type.GetGenericTypeDefinition().Equals(typeof(Nullable<>));
+        }
+
+        public static bool IsGeneric(this Type type, params Type[] parameters) {
+            if (type?.IsGenericType ?? false) {
+                if (parameters.Length == 0) {
+                    return true;
+                }
+                var types = type.GetGenericArguments();
+                if (types.Length != parameters.Length) {
+                    return false;
+                }
+                for (var i = 0; i < types.Length; i++) {
+                    if (types[i] != parameters[i]) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsGeneric<T>(this Type type) {
+            return type.IsGeneric(typeof(T));
+        }
+
+        public static bool Is<T>(this Type type) {
+            return type.Is(typeof(T));
+        }
+
+        public static bool Is(this Type type, Type root) {
+            return type != null && root != null &&
+                (root.IsAssignableFrom(type) ||
+                (type.IsGenericType && type.GetGenericTypeDefinition() == root)
+                );
+        }
+
+        public static object GetDefaultValue(this Type type) {
+            return type.IsValueType ? Activator.CreateInstance(type) : null;
+        }
+
+        public static bool HasConstructor(this Type type, params Type[] types) {
+            return types == null || types.Length == 0
+                ? type.HasDefaultConstructor()
+                : type.GetConstructor(types) != null;
+        }
+
+        public static bool HasDefaultConstructor(this Type type) {
+            return type.GetConstructor(Type.EmptyTypes) != null;
+        }
+
+        public static IReadOnlyList<PropertyInfo> GetAllProperties(this Type type, BindingFlags flags) {
+            var members = new List<PropertyInfo>();
+            members.AddRange(type.GetProperties(flags));
+            if (type.BaseType != null) {
+                members.AddRange(type.BaseType.GetAllProperties(flags));
+            }
+            return members;
+        }
+
+        public static IReadOnlyList<PropertyInfo> GetTopProperties(this Type type) {
+            return type.GetTopProperties(
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.Instance |
+                BindingFlags.NonPublic
+                );
+        }
+
+        public static IReadOnlyList<PropertyInfo> GetTopProperties(this Type type, BindingFlags bindings) {
+            var properties = new List<PropertyInfo>();
+            while (type != null) {
+                foreach (var property in type.GetProperties(bindings)) {
+                    if (!properties.Any(item => item.Name == property.Name)) {
+                        properties.Add(property);
+                    }
+                }
+                type = type.BaseType;
+            }
+            return properties;
+        }
+
+        public static IReadOnlyList<Type> GetLineage(this Type type) {
+            var lineage = new List<Type>();
+            while (type != null) {
+                lineage.Add(type);
+                type = type.BaseType;
+            }
+            return lineage;
+        }
+
+        public static IReadOnlyList<string> GetNamespaces(this Type type) {
+            return type.Namespace.Split('.');
+        }
+    }
+}

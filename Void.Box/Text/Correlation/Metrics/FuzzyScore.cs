508d43a1cb547837ffcae7dc5885049c0df1ae77
The Developer <denis.skornyakov@gmail.com>
2019-07-08 10:36:15 +0300

    Updated FuzzyScore

--- Void.Box/Text/Correlation/Metrics/FuzzyScore.cs
+++ Void.Box/Text/Correlation/Metrics/FuzzyScore.cs
@@ -18,12 +18,18 @@ namespace Void.Text
         
 
         public override double Correlate(string source, string target) {
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(target)) {
+                return default(double);
+            }
             var bound = Math.Min(source.Length, target.Length);
             var score = FuzzyScore.Calculate(source, target);
             return score / (bound + (bound - 1) * 2.0);
         }
 
         public static int Calculate(string source, string target) {
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(target)) {
+                return default(int);
+            }
             var score = default(int);
             int previous = int.MinValue;
             for (int i = 0, j = 0; j < target.Length; j++) {

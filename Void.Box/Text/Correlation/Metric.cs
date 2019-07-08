6a20c5dd464a42b23468bafc14570d3a6b9160e7
The Developer <denis.skornyakov@gmail.com>
2019-07-08 10:31:17 +0300

    Added Metric.GetCost

--- Void.Box/Text/Correlation/Metric.cs
+++ Void.Box/Text/Correlation/Metric.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -8,6 +9,31 @@ namespace Void.Text
     public abstract class Metric
     {
         public abstract double Score(string source, string target);
+
+        public virtual TimeSpan GetCost(string source, string target, int iterations = 100) {
+            iterations = Math.Abs(iterations);
+            iterations++;
+            var timer = new Stopwatch();
+            var lastSum = default(long);
+            var currentSum = default(long);
+            var index = default(int);
+            try {
+                for (index = 0; index < iterations; index++) {
+                    timer.Restart();
+                    Score(source, target);
+                    lastSum = currentSum;
+                    currentSum = checked(currentSum + timer.ElapsedTicks);
+                }
+            }
+            catch (OverflowException) {
+                currentSum = lastSum;
+                index--;
+            }
+            finally {
+                timer.Stop();
+            }
+            return new TimeSpan(currentSum / (index + 1));
+        }
 
         public override bool Equals(object obj) {
             return obj?.GetType() == this.GetType();

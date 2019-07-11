958145623a10fd45628f6307ada90eed4f9e2eff
The Developer <denis.skornyakov@gmail.com>
2019-07-11 23:55:40 +0300

    Added basic test class

---
+++ Void.Selenium.Tests/WebContext.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Void.Selenium.Tests
+{
+    public abstract class WebContext : IDisposable//IAsyncLifetime
+    {
+        protected IWebDriver Driver { get; }
+
+
+
+        public WebContext() {
+            var options = new ChromeOptions();
+            options.AddArgument("headless");
+            this.Driver = new ChromeDriver(options);
+        }
+
+
+        public void Dispose() {
+            this.Driver.Dispose();
+        }
+    }
+}

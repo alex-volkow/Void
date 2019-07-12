7544b3b6a48f0c3063a5fa856fb95683708ce531
The Developer <denis.skornyakov@gmail.com>
2019-07-12 14:07:31 +0300

    Added PropertyExtensions tests

--- Void.Box.Tests/Reflection/PropertiesExtensionsTests.cs
+++ Void.Box.Tests/Reflection/PropertiesExtensionsTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using Xunit;
 
@@ -7,34 +9,78 @@ namespace Void.Reflection
 {
     public class PropertiesExtensionsTests
     {
+        private class Item
+        {
+            private readonly int number = 1;
+
+            public string Text { get; }
+
+            public int IntValue => 123;
+
+            int GetNumer() => this.number;
+        }
+
         [Fact]
         public void GetExistingAutoField() {
-
+            var fields = typeof(Item)
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var text = fields.First(e => e.Name == nameof(Item.Text));
+            Assert.NotNull(text.GetAutoField());
         }
 
         [Fact]
         public void GetNonExistingAutoField() {
-
+            var fields = typeof(Item)
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var number = fields.First(e => e.Name == nameof(Item.IntValue));
+            Assert.Null(number.GetAutoField());
         }
 
         [Fact]
         public void IsAutoProperty() {
-
+            var fields = typeof(Item)
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var text = fields.First(e => e.Name == nameof(Item.Text));
+            Assert.True(text.IsAuto());
         }
 
         [Fact]
         public void IsNonAutoProperty() {
-
+            var fields = typeof(Item)
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var number = fields.First(e => e.Name == nameof(Item.IntValue));
+            Assert.False(number.IsAuto());
         }
 
         [Fact]
         public void SetForceSuccess() {
-
+            var fields = typeof(Item)
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var text = fields.First(e => e.Name == nameof(Item.Text));
+            var instance = new Item();
+            text.SetForce(instance, "pew pew");
+            Assert.Equal("pew pew", instance.Text);
         }
 
         [Fact]
         public void SetForceFail() {
-
+            var fields = typeof(Item)
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var number = fields.First(e => e.Name == nameof(Item.IntValue));
+            var instance = new Item();
+            Assert.ThrowsAny<Exception>(() => number.SetForce(instance, 4));
         }
     }
 }

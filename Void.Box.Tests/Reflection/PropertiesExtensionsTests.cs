bc02d81fbeff8575ffa3c3275af5d313c250d7fc
The Developer <denis.skornyakov@gmail.com>
2019-07-12 14:00:08 +0300

    Added PropertiesExtensionsTests templates

---
+++ Void.Box.Tests/Reflection/PropertiesExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Void.Reflection
+{
+    public class PropertiesExtensionsTests
+    {
+        [Fact]
+        public void GetExistingAutoField() {
+
+        }
+
+        [Fact]
+        public void GetNonExistingAutoField() {
+
+        }
+
+        [Fact]
+        public void IsAutoProperty() {
+
+        }
+
+        [Fact]
+        public void IsNonAutoProperty() {
+
+        }
+
+        [Fact]
+        public void SetForceSuccess() {
+
+        }
+
+        [Fact]
+        public void SetForceFail() {
+
+        }
+    }
+}

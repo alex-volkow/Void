03c5cd789bea3681315ced950a392e2018a00b41
The Developer <denis.skornyakov@gmail.com>
2019-07-12 12:58:57 +0300

    Added FieldsExtensions tests

---
+++ Void.Box.Tests/Reflection/FieldsExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Void.Reflection
+{
+    public class FieldsExtensionsTests
+    {
+        private class Item
+        {
+            private readonly int number = 1;
+
+            public string Text { get; }
+
+            int GetNumer() => this.number;
+        }
+
+        [Fact]
+        public void CheckAutoField() {
+            var fields = typeof(Item)
+                .GetAllFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var autofield = fields.First(e => e.Name.Contains(nameof(Item.Text)));
+            Assert.True(autofield.IsAutoField());
+        }
+
+        [Fact]
+        public void CheckNonAutoField() {
+            var fields = typeof(Item)
+                .GetAllFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var standardField = fields.First(e => !e.Name.Contains(nameof(Item.Text)));
+            Assert.False(standardField.IsAutoField());
+        }
+
+        [Fact]
+        public void GetAutoPropertyName() {
+            var fields = typeof(Item)
+                .GetAllFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                .ToArray();
+            Assert.Equal(2, fields.Length);
+            var autofield = fields.First(e => e.Name.Contains(nameof(Item.Text)));
+            Assert.Equal(nameof(Item.Text), autofield.GetAutoPropertyName());
+        }
+    }
+}

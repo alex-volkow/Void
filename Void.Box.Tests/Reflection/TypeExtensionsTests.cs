a92c3949fc3cd3dc5084db943f48a38b29be8df9
The Developer <denis.skornyakov@gmail.com>
2019-07-12 12:45:44 +0300

    Updated Void.Box.Reflection tests

--- Void.Box.Tests/Reflection/TypeExtensionsTests.cs
+++ Void.Box.Tests/Reflection/TypeExtensionsTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 using Xunit;
 
@@ -31,7 +33,68 @@ namespace Void.Reflection
             Assert.True(typeof(List<IItem>).Is(typeof(List<>)));
         }
 
-        private class Item : IItem { }
+        [Fact]
+        public void GetAllFields() {
+            var item1 = new Item();
+            var item2 = new ChildItem();
+            var item3 = new ChildChildItem();
+            var fields1 = item1.GetType()
+                .GetAllFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            var fields2 = item2.GetType()
+                .GetAllFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            var fields3 = item3.GetType()
+                .GetAllFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            Assert.Single(fields1);
+            Assert.Equal(4, fields2.Length);
+            Assert.Equal(7, fields3.Length);
+        }
+
+        [Fact]
+        public void GetAllProperties() {
+            var item1 = new Item();
+            var item2 = new ChildItem();
+            var item3 = new ChildChildItem();
+            var itemProperties1 = item1.GetType()
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            var itemProperties2 = item2.GetType()
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            var itemProperties3 = item3.GetType()
+                .GetAllProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            Assert.Single(itemProperties1);
+            Assert.Equal(3, itemProperties2.Length);
+            Assert.Equal(4, itemProperties3.Length);
+        }
+
+        [Fact]
+        public void GetTopProperties() {
+            var item1 = new Item();
+            var item2 = new ChildItem();
+            var item3 = new ChildChildItem();
+            var itemProperties1 = item1.GetType()
+                .GetTopProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            var itemProperties2 = item2.GetType()
+                .GetTopProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            var itemProperties3 = item3.GetType()
+                .GetTopProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .ToArray();
+            Assert.Single(itemProperties1);
+            Assert.Equal(2, itemProperties2.Length);
+            Assert.Equal(2, itemProperties3.Length);
+        }
+
+        private class Item : IItem
+        {
+            protected string text;
+            protected virtual string Value => "1";
+        }
 
         private interface IItem { }
 
@@ -41,5 +104,19 @@ namespace Void.Reflection
 
         private interface IStdItems<T> { }
 
+        private class ChildItem : Item
+        {
+            private readonly double f;
+            public object obj;
+            protected override string Value => "2";
+
+            private int Numer => 4;
+        }
+
+        private class ChildChildItem : ChildItem
+        {
+            public int n;
+            protected override string Value => "3";
+        }
     }
 }

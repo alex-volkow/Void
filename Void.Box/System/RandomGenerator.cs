b2f4759a04fb02ae78b512351dc6ebfe7c3a46e2
The Developer <denis.skornyakov@gmail.com>
2019-07-13 23:04:49 +0300

    Added documenation to RandomGenerator

--- Void.Box/System/RandomGenerator.cs
+++ Void.Box/System/RandomGenerator.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Void
 {
+    /// <summary>
+    /// Extends base functional of Random.
+    /// </summary>
     public sealed class RandomGenerator : Random
     {
         public static RandomGenerator Default { get; } = new RandomGenerator((int)(DateTime.Now.Ticks >> 32));
@@ -19,23 +22,38 @@ namespace Void
         }
 
 
-
+        /// <summary>
+        /// Receive random boolean value.
+        /// </summary>
         public bool NextBool() {
             return Next(2) == 0;
         }
 
+        /// <summary>
+        /// Receive random integer +1 or -1.
+        /// </summary>
         public int NextIntSign() {
             return NextBool() ? 1 : -1;
         }
 
+        /// <summary>
+        /// Receive random deoble +1.0 or -1.0.
+        /// </summary>
+        /// <returns></returns>
         public double NextDoubleSign() {
             return NextBool() ? 1.0 : -1.0;
         }
 
+        /// <summary>
+        /// Select random char from array.
+        /// </summary>
         public char NextChar(params char[] chars) {
             return NextChar((IEnumerable<char>)chars);
         }
 
+        /// <summary>
+        /// Select random char from enumeration.
+        /// </summary>
         public char NextChar(IEnumerable<char> chars) {
             if (chars == null || chars.Count() == 0) {
                 throw new InvalidOperationException(

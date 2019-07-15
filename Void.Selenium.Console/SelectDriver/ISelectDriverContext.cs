adc46b05b39420c12b283b3879c8408fffb75b31
The Developer <denis.skornyakov@gmail.com>
2019-07-15 17:06:29 +0300

    Added SelectDriverPage base

---
+++ Void.Selenium.Console/SelectDriver/ISelectDriverContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void.Selenium.Console
+{
+    public interface ISelectDriverContext
+    {
+        FileInfo GetChromedriver();
+        FileInfo GetGekodriver();
+        FileInfo GetTorExecutable();
+    }
+}

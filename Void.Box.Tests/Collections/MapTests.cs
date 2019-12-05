d87ff4b73347741798aeda4e8db572cd0eb96b08
Alex <lex.volkow@gmail.com>
2019-12-05 22:47:36 +0300

    Added Map tests

---
+++ Void.Box.Tests/Collections/MapTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void.Collections
+{
+    [Parallelizable]
+    public class MapTests
+    {
+        [Test]
+        public void AddNullValue() {
+            var map = new Map<int, string>();
+            for (var i = 0; i < 5; i++) {
+                map[i] = i.ToString();
+            }
+            map.Add(5, null);
+            Assert.AreEqual(5, map.Count);
+            map[6] = null;
+            Assert.AreEqual(5, map.Count);
+        }
+
+        [Test]
+        public void SetNullValue() {
+            var map = new Map<int, string>();
+            for (var i = 0; i < 5; i++) {
+                map[i] = i.ToString();
+            }
+            map[0] = null;
+            Assert.AreEqual(4, map.Count);
+        }
+    }
+}

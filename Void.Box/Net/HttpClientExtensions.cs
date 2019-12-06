8a5f45d01bb1fa1498a7b5a8dbaa848eb35b1dc9
Alex <lex.volkow@gmail.com>
2019-12-06 15:17:54 +0300

    Removed artefacts from HttpClientExtensions

--- Void.Box/Net/HttpClientExtensions.cs
+++ Void.Box/Net/HttpClientExtensions.cs
@@ -32,34 +32,5 @@ namespace Void.Net
             }
             return null;
         }
-
-        public static Task<HttpResponseMessage> GetRelative(this HttpClient client, string uri, CancellationToken token) {
-            return client.BaseAddress != null
-                && !string.IsNullOrEmpty(client.BaseAddress.PathAndQuery)
-                && client.BaseAddress.PathAndQuery != "/"
-                ? client.GetAsync(client.CreateRelative(uri).Uri, token)
-                : client.GetAsync(uri, token);
-        }
-
-        public static Task<HttpResponseMessage> PostRelative(this HttpClient client, string uri, HttpContent content,
-            CancellationToken token) {
-            return client.BaseAddress != null
-                && !string.IsNullOrEmpty(client.BaseAddress.PathAndQuery)
-                && client.BaseAddress.PathAndQuery != "/"
-                ? client.PostAsync(client.CreateRelative(uri).Uri, content, token)
-                : client.PostAsync(uri, content, token);
-        }
-
-        private static UriBuilder CreateRelative(this HttpClient client, string uri) {
-            var builder = new UriBuilder(client.BaseAddress);
-            var parameters = uri.Split('?');
-            if (!string.IsNullOrEmpty(parameters[0])) {
-                builder.AppendPath(parameters[0]);
-            }
-            if (parameters.Length == 2 && !string.IsNullOrEmpty(parameters[1])) {
-                builder.AppendQuery(parameters[1]);
-            }
-            return builder;
-        }
     }
 }

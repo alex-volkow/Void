ba4b815bec5d5bf89cb58205dea294bb774038ce
Alex <lex.volkow@gmail.com>
2019-12-09 19:10:05 +0300

    Added Dotnet.GetSolutionsRecursive

---
+++ Void.Box/Diagnostics/Dotnet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Void.IO;
+using Void.Collections;
+
+namespace Void.Diagnostics
+{
+    public static class Dotnet
+    {
+        private static readonly string SOLUTION_FILE_EXTENSION = "sln";
+        private static readonly string PROJECT_FILE_EXTENSION = "csproj";
+
+        /// <summary>
+        /// Find all solution files in sub-folders and all parent folders. 
+        /// Uses application entry point directory for start searching.
+        /// </summary>
+        /// <returns>Ordered file collection.</returns>
+        public static IEnumerable<FileInfo> GetSolutionsRecursive() {
+            return GetSolutionsRecursive(Files.EntryPoint.Directory);
+        }
+
+        /// <summary>
+        /// Find all solution files in sub-folders and all parent folders.
+        /// </summary>
+        /// <param name="location">Start directory.</param>
+        /// <returns>Ordered file collection.</returns>
+        public static IEnumerable<FileInfo> GetSolutionsRecursive(DirectoryInfo location) {
+            if (location == null) {
+                throw new ArgumentNullException(nameof(location));
+            }
+            var extension = $"*.{SOLUTION_FILE_EXTENSION}";
+            return GetFilesRecursiveUp(location, extension)
+                .Union(GetFilesRecursiveDown(location, extension))
+                .Select(e => e.FullName)
+                .ToHashSet()
+                .OrderBy(e => e)
+                .Select(e => new FileInfo(e));
+        }
+
+        private static HashSet<T> ToHashSet<T>(this IEnumerable<T> collection) {
+            return new HashSet<T>(collection);
+        }
+
+        private static List<FileInfo> GetFilesRecursiveDown(DirectoryInfo location, string extension) {
+            var files = location.GetFiles(extension).ToList();
+            foreach (var directory in location.GetDirectories()) {
+                files.AddRange(GetFilesRecursiveDown(location, extension));
+            }
+            return files;
+        }
+
+        private static List<FileInfo> GetFilesRecursiveUp(DirectoryInfo location, string extension) {
+            var directory = location;
+            var files = new List<FileInfo>();
+            while (directory != null) {
+                files.AddRange(directory.GetFiles(extension));
+                directory = directory.Parent;
+            }
+            return files;
+        }
+    }
+}

2378e8278ef468fea034aed3533fcbf7b7218bc3
Alex <lex.volkow@gmail.com>
2019-12-10 13:03:25 +0300

    Added ExceptionEventArgs

---
+++ Void.Box/System/ExceptionEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void
+{
+    public class ExceptionEventArgs : EventArgs
+    {
+        public Exception Exception { get; }
+
+        public ExceptionEventArgs(Exception exception) {
+            this.Exception = exception ?? throw new ArgumentNullException(
+                nameof(exception)
+                );
+        }
+    }
+}

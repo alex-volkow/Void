952f7a11cfe9e091943534c0f7edfe65ee5db8da
Alex <lex.volkow@gmail.com>
2019-12-10 10:17:45 +0300

    Added docs to IProjectArtefacts

--- Void.Box/Diagnostics/IProjectArtefacts.cs
+++ Void.Box/Diagnostics/IProjectArtefacts.cs
@@ -8,16 +8,52 @@ using Void.IO;
 
 namespace Void.Diagnostics
 {
-    public interface IProjectArtefacts : IDisposable
+    /// <summary>
+    /// Represents API to access to project artefacts.
+    /// </summary>
+    public interface IProjectArtefacts
     {
+        /// <summary>
+        /// Project file.
+        /// </summary>
         FileInfo Project { get; }
+
+        /// <summary>
+        /// Application entry point file or null.
+        /// </summary>
         FileInfo EntryPoint { get; }
+
+        /// <summary>
+        /// Location of artefacts files.
+        /// </summary>
         DirectoryInfo Location { get; }
 
+
+        /// <summary>
+        /// Create new isntance of ProcessStartInfo with entry point.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">No entry point.</exception>
         ProcessStartInfo GetStartInfo();
+
+        /// <summary>
+        /// Get artefact files recursive.
+        /// </summary>
         IEnumerable<FileInfo> GetFiles();
+
+        /// <summary>
+        /// Get artefact entries recursive.
+        /// </summary>
         IEnumerable<IEntryReader> GetEntries();
+
+        /// <summary>
+        /// Fend all *.exe files in the artefacts location.
+        /// </summary>
+        /// <returns></returns>
         IEnumerable<FileInfo> GetExecutableFiles();
+
+        /// <summary>
+        /// Delete the artefact location directory with all content.
+        /// </summary>
         void Delete();
     }
 }

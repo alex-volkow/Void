a8767059ad386335e61cb77a23e732c74cd5f52f
Alex <lex.volkow@gmail.com>
2019-12-10 14:32:32 +0300

    Added ErrorConverter

---
+++ Void.Json/Converters/ErrorConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Void.Reflection;
+
+namespace Void.Json.Converters
+{
+    public class ErrorConverter : ValueConverter<IError>
+    {
+        public override IError Deserialize(JToken json, Type type, JsonSerializer serializer) {
+            var proxy = json.ToObject<Proxy>(serializer);
+            var errorType = Type.GetType(proxy.Type) ?? throw new JsonException(
+                $"Type '{proxy.Type}' is no found"
+                );
+            return new ErrorInfo(errorType, proxy.Message, proxy.Content);
+        }
+
+        public override JToken Serialize(IError value, JsonSerializer serializer) {
+            var proxy = new Proxy { 
+                Type = value.Type.GetNameWithAssemblies(),
+                Message = value.Message,
+                Content = value.Content
+            };
+            return JToken.FromObject(proxy, serializer);
+        }
+
+        private class Proxy 
+        {
+            [JsonProperty(Required = Required.Always)]
+            public string Type { get; set; }
+
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public string Message { get; set; }
+
+            [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+            public string Content { get; set; }
+        }
+    }
+}

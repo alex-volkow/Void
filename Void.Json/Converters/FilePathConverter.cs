dca59e4ce8cabe8341d1a3630e8d6deec58f19e8
Alex <lex.volkow@gmail.com>
2020-01-03 12:42:15 +0300

    Added FilePathConverter

---
+++ Void.Json/Converters/FilePathConverter.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Void.IO;
+
+namespace Void.Json.Converters
+{
+    public class FilePathConverter : ValueConverter<FilePath>
+    {
+        public override FilePath Deserialize(JToken json, Type type, JsonSerializer serializer) {
+            return new FilePath((string)json);
+        }
+
+        public override JToken Serialize(FilePath value, JsonSerializer serializer) {
+            return JToken.FromObject(value.ToString());
+        }
+    }
+}

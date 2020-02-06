88a034bc9908c2c1910b1961f335a02d18f10ab4
Alex <lex.volkow@gmail.com>
2020-02-06 12:59:13 +0200

    Added StringBuilderExtensions.TrimEnd

---
+++ Void.Box/Text/StringBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void
+{
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder TrimEnd(this StringBuilder builder) {
+            if (builder == null || builder.Length == 0) {
+                return builder;
+            }
+            var i = builder.Length - 1;
+            while (i >= 0) {
+                if (!char.IsWhiteSpace(builder[--i])) {
+                    break;
+                }
+            }
+            if (i < builder.Length - 1) {
+                builder.Length = i + 1;
+            }
+            return builder;
+        }
+    }
+}

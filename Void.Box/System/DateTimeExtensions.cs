6e0d3a3596ab5195264b6fbf99bc6566509b09ee
Alex <lex.volkow@gmail.com>
2020-02-13 12:43:44 +0200

    Added DateTimeExtensions.Truncate

---
+++ Void.Box/System/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Void
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime Truncate(this DateTime timestamp, TimeUnit unit) {
+            if (timestamp == DateTime.MinValue || timestamp == DateTime.MaxValue) { 
+                return timestamp; 
+            }
+            var interval = new TimeInterval(1, unit);
+            return timestamp.AddTicks(-(timestamp.Ticks % interval.ToTicks()));
+        }
+    }
+}

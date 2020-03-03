8c4971593b505b64f7934222393b7fecaeb8668e
Alex Volkov <lex.volkow@gmail.com>
2020-03-03 15:16:06 +0300

    Updated CancellationTokenManager tests

--- Void.Box.Tests/System/CancellationTokenManagerTests.cs
+++ Void.Box.Tests/System/CancellationTokenManagerTests.cs
@@ -28,5 +28,46 @@ namespace Void
             Assert.True(cancel.IsCancellationRequested);
             Assert.False(cancel.IsDisposed);
         }
+
+        [Test]
+        public void AlreadyCanceled() {
+            using var cancel = new CancellationTokenManager();
+            cancel.Cancel();
+            Assert.ThrowsAsync<TaskCanceledException>(() => Task.Delay(100, cancel.Token));
+            Assert.True(cancel.IsCancellationRequested);
+            Assert.False(cancel.IsDisposed);
+        }
+
+        [Test]
+        public void CancelAfter() {
+            using var cancel = new CancellationTokenManager();
+            var delay = Task.Delay(100, cancel.Token);
+            cancel.CancelAfter(TimeSpan.FromMilliseconds(20));
+            Assert.ThrowsAsync<TaskCanceledException>(async () => {
+                await delay;
+            });
+            Assert.True(cancel.IsCancellationRequested);
+            Assert.False(cancel.IsDisposed);
+        }
+
+        [Test]
+        public void AlreadyDisposed() {
+            var cancel = new CancellationTokenManager();
+            cancel.Dispose();
+            Assert.ThrowsAsync<TaskCanceledException>(() => Task.Delay(100, cancel.Token));
+            Assert.True(cancel.IsCancellationRequested);
+            Assert.True(cancel.IsDisposed);
+        }
+
+        [Test]
+        public async Task DisposeExecuting() {
+            var cancel = new CancellationTokenManager();
+            var delay = Task.Delay(1000, cancel.Token);
+            await Task.Delay(50);
+            cancel.Dispose();
+            Assert.ThrowsAsync<TaskCanceledException>(async () => await delay);
+            Assert.True(cancel.IsCancellationRequested);
+            Assert.True(cancel.IsDisposed);
+        }
     }
 }

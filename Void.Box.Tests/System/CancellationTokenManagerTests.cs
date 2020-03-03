9f33de3e59d2f09914dccf6f910288ddf059966f
Alex Volkov <lex.volkow@gmail.com>
2020-03-03 15:01:51 +0300

    Added CancellationTokenManagerTests

---
+++ Void.Box.Tests/System/CancellationTokenManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void
+{
+    [Parallelizable]
+    public class CancellationTokenManagerTests
+    {
+        [Test]
+        public async Task NormalExecuting() {
+            using var cancel = new CancellationTokenManager();
+            await Task.Delay(100, cancel.Token);
+            Assert.False(cancel.IsCancellationRequested);
+            Assert.False(cancel.IsDisposed);
+        }
+
+        [Test]
+        public void CancelExecuting() {
+            using var cancel = new CancellationTokenManager();
+            var delay = Task.Delay(100, cancel.Token);
+            cancel.Cancel();
+            Assert.ThrowsAsync<TaskCanceledException>(async () => {
+                await delay;
+            });
+            Assert.True(cancel.IsCancellationRequested);
+            Assert.False(cancel.IsDisposed);
+        }
+    }
+}
